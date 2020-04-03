22d0c5243ea1012d7c953d61ad9164c4c2697db1
johnconnor77 <878@holbertonschool.com>
2020-04-02 21:17:28 -0500

    Update functions for MySQL connection

--- models/state.py
+++ models/state.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
     """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship(City, cascade="delete", backref="state")
+    else:
+        @property
+        def cities(self):
+            """returns a list of city"""
+            all_cities = models.storage.all(City)
+            list_cities = []
+            for city in citie_s.values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
