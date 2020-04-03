bdd3955547aab2d450c5b9772b3cab6a00d7df70
johnconnor77 <878@holbertonschool.com>
2020-04-02 23:16:25 -0500

    Update User for sqlalchemy

--- models/user.py
+++ models/user.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
 
 
-class User(BaseModel):
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+
+
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -11,7 +14,9 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
