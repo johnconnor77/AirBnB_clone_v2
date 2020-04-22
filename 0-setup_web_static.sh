2f03aa44a1442c9796b5729437dbb4de1da20e65
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-21 23:03:59 -0500

    Change dir to sites-available and line to sed locate input

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,31 +8,32 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 # Create a fake HTML file (with simple content,
 # to test your Nginx configuration)
-sudo echo -e '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello to all</title>
-</head>
-<body>
-<h1>Config web server</h1>
-</body>
-</html>' | sudo tee /data/web_static/releases/test/index.html
+sudo echo -e "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
 # Create a symbolic link /data/web_static/current linked to the 
 # /data/web_static/releases/test/ folder. If the symbolic link
 # already exists, it should be deleted and recreated every time
 # the script is ran.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 # Give ownership of the /data/ folder to the ubuntu user AND group
 # (you can assume this user and group exist). This should be recursive;
 # everything inside should be created/owned by this user/group.
-sudo chown -R  ubuntu:ubuntu /data
+
+sudo chown -R  ubuntu:ubuntu /data/
 # Update the Nginx configuration to serve the content of
 # /data/web_static/current/ to hbnb_static (ex:
 # https://mydomainname.tech/hbnb_static). 
 # Use alias inside your Nginx configuration
-static="\\\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n}"
-sudo sed -i "40i $static" /etc/nginx/sites-enabled/default
+
+static="\\\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}"
+sudo sed -i "41i $static" /etc/nginx/sites-avaiable/default
+
 # Don’t forget to restart Nginx after updating the configuration:
 sudo service nginx restart
