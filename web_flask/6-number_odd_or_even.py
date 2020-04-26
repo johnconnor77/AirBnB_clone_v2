cabb20148399a264aafe865130a676506dc8f727
johnconnor77 <jfcalleh@unal.edu.co>
2020-04-26 13:38:07 -0500

    Pep8 issues fixed

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,6 +47,7 @@ def number_template(n):
     """
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def num_odd_or_even(n):
     """Display a HTML page only if n is an integer and
